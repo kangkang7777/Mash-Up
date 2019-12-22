6f4129db7fb23342adb5cafed6e21515d30c2cda
Krisskr <546628725@qq.com>
2019-12-23 01:09:30 +0800

    restore

---
+++ js/init.js
@@ -0,0 +1,32 @@
+//
+// $(function()
+// {
+//     // enter
+//     $("#searchTerm").keypress(function(e) {
+//         //回车
+//         if (e.keyCode === 13) {
+//             var searchTerm = $("#searchTerm").val();//输入框内容
+//
+//             window.location.href="main.html";
+//
+//             $("#error").html("");
+//             $("#gralInfo").html("");
+//             $("#cityTitle").html("");
+//             $("#wikiResult").html("");
+//             $("#bingResult").html("");
+//             $("#govResult").html("");
+//             $("#news").html("");
+//             $("#output").hide();
+//
+//             var map = new AMap.Map('map_container', {
+//                 resizeEnable: true, //是否监控地图容器尺寸变化
+//                 zoom: 11, //初始化地图层级
+//                 center: [116.397428, 39.90923] //初始化地图中心点
+//             });
+//             map();
+//
+//         }
+//     });
+// });
+
+
